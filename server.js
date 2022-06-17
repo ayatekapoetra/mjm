01a460334a4131cb24ebb9ab8478f0668c18b9af
Mushawiruddin <angkomuhtar@gmail.com>
2022-06-17 11:35:20 +0800

    sticky panel

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-'use strict'
-require('dotenv').config()
-const { ADONIS_INFO } = require('adonis-npm-project-key')
+"use strict";
+require("dotenv").config();
+const { ADONIS_INFO } = require("adonis-npm-project-key");
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +18,18 @@ const { ADONIS_INFO } = require('adonis-npm-project-key')
 |     Also you can preload files by calling `preLoad('path/to/file')` method.
 |     Make sure to pass relative path from the project root.
 */
-const { Ignitor } = require('@adonisjs/ignitor')
-KEY_APP()
-async function KEY_APP(){
-  if(await ADONIS_INFO(process.env.APP_NAME, process.env.APP_SERIAL, process.env.APP_TOKEN))
-  new Ignitor(require('@adonisjs/fold'))
-  .appRoot(__dirname)
-  .fireHttpServer()
-  .catch(console.error)
+const { Ignitor } = require("@adonisjs/ignitor");
+KEY_APP();
+async function KEY_APP() {
+  if (
+    await ADONIS_INFO(
+      process.env.APP_NAME,
+      process.env.APP_SERIAL,
+      process.env.APP_TOKEN
+    )
+  )
+    new Ignitor(require("@adonisjs/fold"))
+      .appRoot(__dirname)
+      .fireHttpServer()
+      .catch(console.error);
 }
